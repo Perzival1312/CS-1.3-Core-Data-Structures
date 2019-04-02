551f144a74cdd9af4ccf3868addc88eb0b5bc652
Perzival1312 <jj.mongoose11@hotmail.com>
2019-04-01 22:25:52 -0700

    restructured

--- source/bases.py
+++ source/bases.py
@@ -9,6 +9,8 @@ import string
 # string.ascii_letters is ascii_lowercase + ascii_uppercase
 # string.printable is digits + ascii_letters + punctuation + whitespace
 
+num_string_upper = string.digits + string.ascii_uppercase
+num_string_lower = string.digits + string.ascii_lowercase
 
 def decode(digits, base):
     """Decode given digits in given base to number in base 10.
@@ -17,12 +19,26 @@ def decode(digits, base):
     return: int -- integer representation of number (in base 10)"""
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
-    # TODO: Decode digits from binary (base 2)
-    # ...
-    # TODO: Decode digits from hexadecimal (base 16)
-    # ...
-    # TODO: Decode digits from any base (2 up to 36)
-    # ...
+
+    result = 0
+    dig_arr = []
+    current_num_string_upper = num_string_upper[0:base]
+    current_num_string_lower = num_string_lower[0:base]
+
+    # turn number into array
+    for digit in str(digits):
+        dig_arr.append(digit)
+    # reverse the array so that the index is the power of the base
+    dig_arr = dig_arr[::-1]
+    # create the base 10 number
+    for ind, digit in enumerate(dig_arr):
+        if current_num_string_upper.__contains__(digit):
+            result += int(current_num_string_upper.index(digit)) * (base**ind)
+        elif current_num_string_lower.__contains__(digit):
+            result += int(current_num_string_lower.index(digit)) * (base**ind)
+        else:
+            raise ValueError('{} is not a valid number in base {}'.format(digits, base))
+    return str(result)
 
 
 def encode(number, base):
@@ -40,6 +56,39 @@ def encode(number, base):
     # ...
     # TODO: Encode number in any base (2 up to 36)
     # ...
+    number += 1
+    length = 0
+    current_digit_set = num_string_upper[0:base]
+    # gets the length of the number in the new base
+    for i in range(number):
+        if base**i < number:
+            length += 1
+        else:
+            break
+    # Creates an array for the new number
+    new_num = ['0' for _ in range(length)]
+
+    # for each digit in new num to find that digit
+    for ind in range(len(new_num)):
+        # checkand find larget possible digit that works
+        for possibility in reversed(current_digit_set):
+            # get possible number
+            num = int(current_digit_set.index(possibility)) * (base**ind)
+            # if possible number is less than input number
+            # meaning that the specified digit can work in 
+            # specified position.
+            if num < number:
+                new_num[ind] = possibility
+                # decrement input number by added number
+                # in newly created number
+                number -= num
+                # stop checking possibilities
+                break
+        print(new_num)
+    
+    print(''.join(new_num))
+
+
 
 
 def convert(digits, base1, base2):
@@ -59,6 +108,7 @@ def convert(digits, base1, base2):
     # ...
     # TODO: Convert digits from any base to any base (2 up to 36)
     # ...
+    return 0
 
 
 def main():
@@ -72,6 +122,11 @@ def main():
         # Convert given digits between bases
         result = convert(digits, base1, base2)
         print('{} in base {} is {} in base {}'.format(digits, base1, result, base2))
+
+    elif len(args) == 2:
+        # print(encode(int(args[0]), int(args[1])))
+        print(decode(args[0], int(args[1])))
+    
     else:
         print('Usage: {} digits base1 base2'.format(sys.argv[0]))
         print('Converts digits from base1 to base2')
