1ceea42eed8a800436abec596083e88c99f97149
Perzival1312 <jj.mongoose11@hotmail.com>
2019-04-02 15:27:44 -0700

    cleanup

--- source/bases.py
+++ source/bases.py
@@ -82,7 +82,7 @@ def encode(number, base):
                 break
     
     result = ''.join(new_num)
-    return result    
+    return result
 
 
 
@@ -96,14 +96,7 @@ def convert(digits, base1, base2):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
     assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
-    # TODO: Convert digits from base 2 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 2 to base 10 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 10 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from any base to any base (2 up to 36)
-    # ...
+
     return encode(decode(digits, base1), base2)
 
 
