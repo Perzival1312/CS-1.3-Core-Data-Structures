4484608e1caee42a178540e3df1499ef1487253b
Perzival1312 <jj.mongoose11@hotmail.com>
2019-04-03 21:03:52 -0700

    binary-iter working

--- source/search.py
+++ source/search.py
@@ -1,4 +1,5 @@
 #!python
+import math
 
 def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
@@ -18,7 +19,8 @@ def linear_search_iterative(array, item):
 
 def linear_search_recursive(array, item, index=0):
     # TODO: implement linear search recursively here
-    pass
+    return linear_search_recursive(array, item, index+1)
+    # pass
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests
 
@@ -33,7 +35,29 @@ def binary_search(array, item):
 
 def binary_search_iterative(array, item):
     # TODO: implement binary search iteratively here
-    pass
+    
+    index = len(array) // 2
+    if item not in set(array):
+        return None
+    while True:
+        if item == array[index]:
+            return index
+        else:
+            if array[index] > item:
+                index = index // 2
+            else:
+                index = (index + len(array)) // 2
+        print(index)
+
+    # if equal return index
+    # else
+    #   if more than 
+    #       next_area = index//2
+    #   else #less than
+    #       next_area = math.floor(index * 1.5)
+    # 
+    # if next_area//2 == next_area --> return None.
+
     # once implemented, change binary_search to call binary_search_iterative
     # to verify that your iterative implementation passes all tests
 
@@ -43,3 +67,15 @@ def binary_search_recursive(array, item, left=None, right=None):
     pass
     # once implemented, change binary_search to call binary_search_recursive
     # to verify that your recursive implementation passes all tests
+
+
+def main():
+    names = ['Winnie', 'Kojin', 'Brian', 'Nabil', 'Julia', 'Alex', 'Nick']
+    contain = 'Julia'
+    not_contain = 'Jeremy'
+
+    print(binary_search(names, contain))
+    print(binary_search(names, not_contain))
+
+if __name__ == '__main__':
+    main()
